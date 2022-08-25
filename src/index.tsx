9432d662552a28ae12b2568a2959b60dd8bc9d2d
Robert Winterbottom <robertwinterbottom@Bubbles-MBP.fios-router.home>
2022-08-25 00:18:25 -0400

    General cleanup, added routing, scss, and more foundation code
    Signed-off-by: Robert Winterbottom <robertwinterbottom@Bubbles-MBP.fios-router.home>

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,14 @@
 import { render } from "solid-js/web";
+import App from './core/App';
 
-function App() {
-  return (
-    <>
-      <header>Hello</header>
-      <main>Testing</main>
-      <footer>FOOTER</footer>
-    </>
-  );
-}
+import './index.scss';
 
 /**
   * @function launch
   * @description Initialize the app when the dom is loaded 
   */
 function launch() {
-  render(App, document.getElementById("app-root"));
+  render(() => <App />, document.getElementById("app-root"));
 }
 
 if (document.readyState === "complete") {
